f5be7cf7f2fec29ab797e73737498501546f6a39
GBFerro <giovani.ferro@usp.br>
2023-03-09 11:41:41 -0300

    refatorado para utilizar em matrizes nao quadradas

--- MatrizInversa/Program.cs
+++ MatrizInversa/Program.cs
@@ -1,22 +1,25 @@
-﻿int[,] table = new int[3, 3];
-int[,] aux = new int[3, 3];
+﻿int[,] table = new int[10, 6];
+int[,] aux = new int[10, 6];
 
-for (int i = 0; i < 3; i++)
+int contaLinha = table.GetLength(0);
+int contaColuna = table.GetLength(1);
+
+for (int row = 0; row < contaLinha; row++)
 {
-    for (int j = 0; j < 3; j++)
+    for (int col = 0; col < contaColuna; col++)
     {
-        table[i, j] = (i+1)*(j+1);
-        aux[2-i, 2-j] = table[i, j];
-        Console.Write(table[i, j]);
+        table[row, col] = (row + 1) * (col + 1);
+        aux[(contaLinha - 1) - row, (contaColuna - 1) - col] = table[row, col];
+        Console.Write(table[row, col] + " | ");
     }
     Console.WriteLine("\n");
 }
 Console.WriteLine("\n");
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < contaLinha; i++)
 {
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < contaColuna; j++)
     {
-        Console.Write(aux[i,j]);
+        Console.Write(aux[i, j] + " | ");
     }
     Console.WriteLine("\n");
 }
